2118c79250fa491f7cfe2967c7368f2239428923
userfiona <fionaramaesela.com>
2024-01-16 21:30:22 +0000

    g

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -7,11 +7,11 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def task_wait_random(max_delay: int = 10) -> asyncio.Future:
     """
-        Args:
-            max_delay: max wait
+    Args:
+        max_delay: max wait
 
-        Return:
-            Task
+    Return:
+        Task
     """
     task = asyncio.create_task(wait_random(max_delay))
-    return taski
+    return task
