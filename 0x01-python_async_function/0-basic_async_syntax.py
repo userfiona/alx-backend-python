e7dbd9dca813617f557204f74dbf8d7091ea5b4d
userfiona <fionaramaesela.com>
2024-01-16 20:48:06 +0000

    f

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,10 +4,9 @@
 import asyncio
 import random
 
-asyc def wait_random(max_delay: int = 10) -> float:
-
+async def wait_random(max_delay: int = 10) -> float:
     """waits random delay between 0 and max delay"""
-    random_number = random.uniform(0,max_delay)
+    random_number = random.uniform(0, max_delay)
 
     await asyncio.sleep(random_number)
-    return random_number
+    return random_number  # Add a colon at the end
